e2f1eafefe1fb91c1f6c116ac303fcf9664fcf49
Your Name <you@example.com>
2023-04-06 12:22:23 +0300

    unuse global
    this version dont use a global variables. But the version has a problem: the first second lasts longer than one second.

--- calculate_bot.py
+++ calculate_bot.py
@@ -7,34 +7,27 @@ load_dotenv()
 TG_TOKEN = os.environ['TG_TOKEN']
 
 
-def parse_time_and_create_countdown(chat_id, question):
-    global time
+def parse_time_and_create_countdown(chat_id, question, bot):
     time = parse(question)
+    message = f"Осталось секунд: {time}"
+    chart = render_progressbar(time, 0)
+    message_id = bot.send_message(chat_id, f'{message}\n{chart}')
     bot.create_countdown(
         time,
         notify_progress,
-        chat_id=chat_id
+        chat_id=chat_id,
+        time=time,
+        bot=bot,
+        message_id=message_id
     )
 
 
-def notify_progress(secs_left, chat_id):
+def notify_progress(secs_left, chat_id, time, bot, message_id):
     message = f"Осталось секунд: {secs_left}"
-    global switch, message_id, progress_message_id
-    if switch:
-        message_id = bot.send_message(chat_id, message)
-        progress_message_id = bot.send_message(
-            chat_id,
-            render_progressbar(time, time - secs_left)
-        )
-        switch = False
-    else:
-        chart = render_progressbar(time, time - secs_left)
-        bot.update_message(chat_id, message_id, message)
-        bot.update_message(chat_id, progress_message_id, chart)
-
+    chart = render_progressbar(time, time - secs_left)
+    bot.update_message(chat_id, message_id, f'{message}\n{chart}')
     if not secs_left:
         bot.send_message(chat_id, "Время вышло")
-        switch = True
 
 
 def render_progressbar(total, iteration, prefix='', suffix='',
@@ -48,10 +41,8 @@ def render_progressbar(total, iteration, prefix='', suffix='',
 
 
 def main():
-    global bot, switch
-    switch = True
     bot = ptbot.Bot(TG_TOKEN)
-    bot.reply_on_message(parse_time_and_create_countdown)
+    bot.reply_on_message(parse_time_and_create_countdown, bot=bot)
     bot.run_bot()
 
 
