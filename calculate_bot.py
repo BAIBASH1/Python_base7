0e71dd083141a8d01ae33c84f9e6116844694455
Your Name <you@example.com>
2023-04-08 22:36:38 +0300

    made corrections

--- calculate_bot.py
+++ calculate_bot.py
@@ -4,29 +4,25 @@ from pytimeparse import parse
 from dotenv import load_dotenv
 
 
-load_dotenv()
-TG_TOKEN = os.environ['TG_TOKEN']
-
-
 def parse_time_and_create_countdown(chat_id, question, bot):
     time = parse(question)
+    chart = render_progressbar(time, 0)
+    message = f"Осталось секунд: {time}"
+    message_id = bot.send_message(chat_id, f'{message}\n{chart}')
     bot.create_countdown(
         time,
         notify_progress,
         chat_id=chat_id,
         time=time,
-        bot=bot
+        bot=bot,
+        message_id=message_id
     )
 
 
-def notify_progress(secs_left, chat_id, time, bot):
+def notify_progress(secs_left, chat_id, time, bot, message_id):
     message = f"Осталось секунд: {secs_left}"
     chart = render_progressbar(time, time - secs_left)
-    global message_id
-    if not (time - secs_left):
-        message_id = bot.send_message(chat_id, f'{message}\n{chart}')
-    else:
-        bot.update_message(chat_id, message_id, f'{message}\n{chart}')
+    bot.update_message(chat_id, message_id, f'{message}\n{chart}')
     if not secs_left:
         bot.send_message(chat_id, "Время вышло")
 
@@ -42,6 +38,8 @@ def render_progressbar(total, iteration, prefix='', suffix='',
 
 
 def main():
+    load_dotenv()
+    TG_TOKEN = os.environ['TG_TOKEN']
     bot = ptbot.Bot(TG_TOKEN)
     bot.reply_on_message(parse_time_and_create_countdown, bot=bot)
     bot.run_bot()
